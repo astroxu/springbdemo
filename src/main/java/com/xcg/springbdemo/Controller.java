7234f27b19376b6005fb509a21f1a6f244336d9a
chengang <chenganggo@gmail.com>
2018-06-19 16:59:35 +0800

    6月19日 16：59 ams-15555

--- src/main/java/com/xcg/springbdemo/Controller.java
+++ src/main/java/com/xcg/springbdemo/Controller.java
@@ -1,20 +1,32 @@
 package com.xcg.springbdemo;
 
 import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @Author: CHENGANGGO@GMAIL.COM
  * @Date: 2018/5/27 20:14
  */
 @RestController
-@RequestMapping("/springbdemo/")
+@RequestMapping("/springbdemo")
 public class Controller {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Value("${name}")
     private String name;
 
@@ -24,9 +36,12 @@ public class Controller {
     @Value("${content}")
     private String content;
 
-    @RequestMapping(value = "say", method = RequestMethod.GET)
+    @RequestMapping(value = "/say", method = RequestMethod.GET)
     public String say() {
+        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
+        logger.info("/say: host=" + serviceInstance.getHost()+",service_id="+serviceInstance.getServiceId());
         //return "Hello Spring Boot!";
-        return "Hello Spring Boot! name: " + name + ",age:" + age + "  content:" + content+"热部署";
+        return "Hello World!";
+        //return "Hello Spring Boot! name: " + name + ",age:" + age + "  content:" + content+"热部署";
     }
 }
--- src/main/java/com/xcg/springbdemo/SpringbdemoApplication.java
+++ src/main/java/com/xcg/springbdemo/SpringbdemoApplication.java
@@ -2,13 +2,16 @@ package com.xcg.springbdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringbdemoApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context= SpringApplication.run(SpringbdemoApplication.class, args);
+        SpringApplication.run(Controller.class,args);
+        //ConfigurableApplicationContext context= SpringApplication.run(Controller.class, args);
         //通过getEnvironment(),getProperty()方法获取配置属性值
         //System.out.println("name: " + context.getEnvironment().getProperty("name") + ",age: "+ context.getEnvironment().getProperty("age"));
     }
